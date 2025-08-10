01dd116f039c811e6709bfc95fbef51503afc0ed
idan <idanu55@gmail.com>
2025-08-10 20:04:34 +0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!! from Jenkins CI/CD pipeline!');
+  res.send('Hello World!! idan king from Jenkins CI/CD pipeline!');
 });
 
 const port = process.env.PORT || 3000;
